5f9e656e0e859f386b0c2b88691c37366a2e20b1
Your Name <you@example.com>
2021-01-13 11:02:10 +0700

    update Lab4-1.php

--- Lab4-1.php
+++ Lab4-1.php
@@ -6,7 +6,7 @@
         $y = 200;
         $z = 300;
         $add = $x + $y;
-        $sub = $z - $y;
+        $sub = $z - $x;
         $multiply = $sub * $add;
         $divide = $multiply / $add;
         $message = "Hello World";
@@ -14,7 +14,7 @@
         echo $y; echo "<br />";
         echo $z; echo "<br />";
         echo "ผลบวก"." ".$x."  +  ".$y."=".$add ; echo "<br />";
-        echo "ผลลบ"." ".$z."  -  ".$y."=".$sub ; echo "<br />";
+        echo "ผลลบ"." ".$z."  -  ".$x."=".$sub ; echo "<br />";
         echo "ผลคูณ"." ".$sub."  *  ".$add."=".$multiply ; echo "<br />";
         echo "ผลหาร"." ".$multiply."  /  ".$add."=".$divide ; echo "<br />";
         echo $message; echo "<br />";

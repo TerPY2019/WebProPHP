22acc290d1492cc76b476ebe052afab8d1830995
Your Name <you@example.com>
2021-01-14 23:16:58 +0700

    craeate test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php 
+            echo(pi());
+            $x = min(5,6,8,9,12,36,14);
+            $y = max(59,625,51,85,3,2,18,69);
+            echo('<h1>'."Min is Numbers ".$y.'</h1>');
+            echo('<h1>'."Max is Numbers ".$x.'</h1>');
+            echo("<h1>"."function ads ".abs(-6.6)."</h1>");
+            echo("<h1>"."function returns the square root of a number"."echo(sqrt(64)) = ".sqrt(64)."</h1>");
+        ?>
+        
+</body>
+</html>

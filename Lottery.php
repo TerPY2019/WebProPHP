5b151fb3011921c1be05538bbe6b3c524c4f8aa9
Ter Name <terkub8142@gmail.com>
2021-02-15 10:27:27 +0700

    update

--- Lottery.php
+++ Lottery.php
@@ -7,25 +7,26 @@
     <title>Document</title>
 </head>
 <style>
-p {
-  font-size: 25px;
-}
 input[type = number]{
     width: 15%;
     height:20px;
+}
+
+body{
+    background: rgb(135,194,253)
 }
 </style>
 <body>
 <center> 
     <form action="Result-lottery.php" method="post">
-    <p>โปรแกรมตรวจเลขท้ายสองตัว</p>
-    รางวัล เมื่อถูกเลขท้าย 2 ตัว (ต้นทุนซื้อ X 60 เท่า)<br><br>
-    เช่น ถ้าซื้อ 10 บาท ถูกรางวัลรับเงิน 600 บาท<br><br>
-    เลขเสี่ยงโชค[0-99]
-        <input type="number" name="numlottery" min=0 max=99 required><br><br><br>
-        * ซื้อ ได้ ตั้งแต่ 10 - 500 บาท เท่านั้น<br><br>
+    <p style = "font-size: 30px; color:#000080; font-weight: bold">โปรแกรมตรวจเลขท้ายสองตัว</p>
+    <p style = "font-size: 19px; color:#FF0000; font-weight: bold">รางวัล เมื่อถูกเลขท้าย 2 ตัว (ต้นทุนซื้อ X 60 เท่า)</p>
+    <p style = "font-size: 19px; color:#FF0000; font-weight: bold"> เช่น ถ้าซื้อ 10 บาท ถูกรางวัลรับเงิน 600 บาท</p>
+    <p style = "font-size: 19px; color:#FF0000; font-weight: bold"> เลขเสี่ยงโชค[0-99]
+        <input type="number" name="numlottery" min=0 max=99 required></p><br><br><br>
+        <p style = "font-size: 30px; color:#E80909; font-weight: bold">* ซื้อ ได้ ตั้งแต่ 10 - 500 บาท เท่านั้น<br><br>
         จำนวนเงินที่คุณต้องการซื้อ [10 - 500 บาท]
-        <input type="number" name="money" min=10 max=500 required><br><br>
+        <input type="number" name="money" min=10 max=500 required></p><br><br>
         <input type="submit" value = "ตรวจรางวัล">
     </form>
 </center>
--- Result-lottery.php
+++ Result-lottery.php
@@ -6,22 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+h3{
+    border-style:solid ;
+    border-width: 5px;
+    border-color: #000000;
+}
+</style>
 <body>
+<h3>
 <?php
     $numlotary = $_POST["numlottery"];
     $money = $_POST["money"];
     $numberCorrect = rand(0,99);
     if($numlotary == $numberCorrect ){
-        echo "<h3>ยินดีด้วยคุณถูกรางวัล"."<br><br>"."คุณได้เงินรางวัล ".$money*60.0."บาท"."<br>";
+        echo "ยินดีด้วยคุณถูกรางวัล"."<br><br>"."คุณได้เงินรางวัล ".$money*60.0."บาท"."<br>";
 
     }else{
-        echo "<h3>เสียใจด้วยคุณไม่ถูกรางวัล<br>";
+        echo "เสียใจด้วยคุณไม่ถูกรางวัล<br>";
     }
 ?>
     <a href="Lottery.php"><input type="button" value="Back"></a><br><br>
 <?php
     echo "รางวัล เลขที่ออกคือ : $numberCorrect"
 ?>
+</h3>
     
     
 </body>
